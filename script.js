e52bc80264c67c214c483a363ccab10b92f01f56
Jonathan Oliveira <jonathanoliveira2@hotmail.com>
2022-11-16 14:56:50 -0300

    atualizacao

---
+++ script.js
@@ -0,0 +1,7 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal(".container-title ", { duration: 2000 });
+
+sr.reveal(".sobre", { duration: 2000 });
+
+sr.reveal(".projeto-container", { duration: 2000 });
